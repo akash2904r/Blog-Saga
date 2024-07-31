589f4967749a5bdeace03a9aaf148d72f5c3f331
Akash <150268192+akash2904r@users.noreply.github.com>
2024-07-31 18:54:37 +0530

    Delete client directory

--- client/src/features/api/apiSlice.ts
+++
@@ -1,8 +0,0 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-
-export const apiSlice = createApi({
-    reducerPath: 'api',
-    baseQuery: fetchBaseQuery({ baseUrl: "https://saga-server-j1ko.onrender.com" }),
-    tagTypes: ["User", "Post", "Comment"],
-    endpoints: builder => ({})
-});
